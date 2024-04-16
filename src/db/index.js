c3a248ce549f3be8c1c14c0858a4ccf63c89797c
Saksham <sakshambisen100@gmail.com>
2024-04-16 19:25:16 +0530

    db connection

---
+++ src/db/index.js
@@ -0,0 +1,36 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "../constants.js";
+
+// const connectDB = async ()=> {
+//     try {
+//        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+//        console.log(`\n MongoDB Connected!! DB HOST : ${connectionInstance.connection.host}`);
+        
+//     } catch (error) {
+//         console.log("MongoDB connection error : ", error);
+//         process.exit(1)
+        
+//     }
+// }
+
+// export default connectDB;
+
+
+import express from "express";
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async() => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        console.log(`\n MONGODB connected!! DB HOST : ${connectionInstance.connection.host}`)
+        
+    } catch (error) {
+        console.log("MONGODB connection error : ", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
